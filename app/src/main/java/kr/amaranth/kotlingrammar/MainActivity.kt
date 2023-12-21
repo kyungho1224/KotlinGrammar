11e95f030ff56f0e85a861faacf3a80cf05b3e76
kimkyungho <dev_kimkyungho@kakao.com>
2023-12-21 23:03:00 +0900

    안드로이드 바인딩 셋팅

--- app/src/main/java/kr/amaranth/kotlingrammar/MainActivity.kt
+++ app/src/main/java/kr/amaranth/kotlingrammar/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.amaranth.kotlingrammar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.amaranth.kotlingrammar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "hi"
+
     }
+
 }
